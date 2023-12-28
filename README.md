428ff6785e2229b2ccb6f22cf348cdddc4c65150
maylique <supercoder@AirvonSperCoder.fritz.box>
2023-12-28 11:09:40 +0100

    bones set

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# MovieDB
+# Movie Database
+
+Diese Movie Database ermöglicht es, Filme mithilfe von JavaScript zu verwalten, zu sortieren und zu filtern.
+
+# Direktlink: https://maylique.github.io/MovieDB/
---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+const search = document.querySelector('#searchButton')
+const sort = document.querySelector('#sortYear')
+const sortReverse = document.querySelector('#sortYearReverse')
+const sortRating = document.querySelector('#sortRating')
+const grid = document.querySelector('.grid')
+
+// const movies <-- Array mit Arrays drin
+
+
+
