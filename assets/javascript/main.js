d16671167d205d36a1f8a295c41fe30814e24434
maylique <supercoder@AirvonSperCoder.fritz.box>
2023-12-28 15:44:28 +0100

    2 jahre upload

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,5 +6,42 @@ const grid = document.querySelector('.grid')
 
 // const movies <-- Array mit Arrays drin
 
+const arrayToDiv = (array) => {
+    array.forEach(array => {
+        let strings = array.toString()
+        const div = document.createElement('div')
+        const title = document.createElement('h2')
+        const year = document.createElement('p')
+        const director = document.createElement('h3')
+        const length = document.createElement('p')
+        const genre = document.createElement('p')
+        const rating = document.createElement('p')
+        grid.appendChild(div)
+        div.appendChild(title)
+        div.appendChild(year)
+        div.appendChild(director)
+        div.appendChild(length)
+        div.appendChild(genre)
+        div.appendChild(rating)
+        array.forEach(element => {
+            title.textContent = array[0]
+            year.textContent = array[1]
+            director.textContent = array[2]
+            length.textContent = array[3]
+            genre.textContent = array[4]
+            rating.textContent = array[5]
+        })
+    });
+}
+arrayToDiv(movies)
 
+
+const sortFunc = () => {
+    movies.sort()
+    grid.innerHTML = ''
+    arrayToDiv(movies)
+}
+
+
+sort.addEventListener('click', sortFunc)
 
